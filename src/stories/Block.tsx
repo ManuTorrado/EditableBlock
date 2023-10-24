078f9f4e9dd148832a6d1b5e4ab3345a14cbd84e
ManuTorrado <manuel.torrado@gmail.com>
2023-10-23 23:46:17 -0300

    makes blocks draggable

--- src/stories/Block.tsx
+++ src/stories/Block.tsx
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { block } from "./EditableBlock";
 
-export const Block = (props: block) => {
-  const id = uuidv4();
+export const Block = ({
+  line,
+  content,
+  handleAdd,
+  type,
+  draggable = true,
+  id,
+  onDragStart,
+  onDragEnter,
+  OnDragEnd,
+  onDragOver,
+}: block) => {
+  const [isDraggable, setDraggable] = useState(draggable);
+  const [isEditable, setEditable] = useState(false);
+  const [numberLine, setNumberLine] = useState(line);
+  const [isHovered, setHovered] = useState(false);
 
-  const handleClick = () => {
-    props.handleAdd({});
+  const handleAddButton = () => {
+    handleAdd({ id: uuidv4() });
   };
 
   return (
     <div
+      onDragOver={onDragOver}
+      onDragEnd={OnDragEnd}
+      onDragStart={onDragStart}
+      onDragEnter={onDragEnter}
+      draggable={isDraggable}
       style={{
         display: "flex",
         justifyContent: "space-around",
+        cursor: "pointer",
+        border: isHovered ? "1px blue solid" : "none",
       }}
       id={id}
     >
-      <button onClick={handleClick}>+</button>
+      <button onClick={handleAddButton}>+</button>
       <div
         style={{
           flexGrow: 1,
           border: "1px solid black",
+          opacity: 1,
         }}
-        contentEditable
+        contentEditable={isEditable}
       >
         {id}
       </div>
--- src/stories/EditableBlock.tsx
+++ src/stories/EditableBlock.tsx
@@ -1,34 +1,100 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Block } from "./Block";
+import { v4 as uuidv4 } from "uuid";
 
-type Callback = (error: Error | null, data: any) => void;
+type Callback = (error: Error | null, data?: any) => void;
 
 export interface block {
   handleAdd: (Callback) => void;
   type?: string;
   content?: string | React.ReactNode;
+  id?: string;
+  line: number;
+  draggable?: boolean;
+  onDragStart?: React.DragEventHandler;
+  onDragEnter?: React.DragEventHandler;
+  OnDragEnd?: React.DragEventHandler;
+  onDragOver?: React.DragEventHandler;
 }
 
-export const EditableBlock = () => {
-  const [blocks, SetBlocks] = useState<Array<block>>([]);
+interface Props {
+  style?: React.CSSProperties;
+  enumerated?: boolean;
+  blocks: Array<block>;
+}
+
+export const EditableBlock = ({ blocks = [], enumerated = true }: Props) => {
+  const [blocksState, SetBlocks] = useState<Array<block>>(blocks);
+  const [isEnumerated, setEnumerated] = useState(enumerated);
+
+  const draggedItem = useRef<number>(0);
+  const draggedOverItem = useRef<number>(0);
 
   const handleAdd = (data: block) => {
-    SetBlocks([...blocks, data]);
+    SetBlocks([...blocksState, data]);
   };
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "stretch",
 
-        border: "1px black dashed",
-      }}
-    >
-      <Block handleAdd={handleAdd} />
-      {blocks.map((block: block) => (
-        <Block handleAdd={handleAdd} />
-      ))}
+  const sortElements = () => {
+    const blocksClone = [...blocksState];
+    const fixClone = blocksClone[draggedItem.current];
+    blocksClone[draggedItem.current] = blocksClone[draggedOverItem.current];
+    blocksClone[draggedOverItem.current] = fixClone;
+    SetBlocks(blocksClone);
+  };
+
+  useEffect(() => {
+    if (blocksState.length == 0) {
+      SetBlocks([
+        {
+          id: uuidv4(),
+          line: 0,
+          handleAdd: handleAdd,
+        },
+      ]);
+    }
+  });
+
+  useEffect(() => {
+    console.log(blocksState);
+  }, [blocksState]);
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "stretch",
+          gap: 5,
+          border: "1px black dashed",
+        }}
+      >
+        {blocksState.map((block: block, key: number) => (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+            }}
+            key={key}
+          >
+            <div style={{ backgroundColor: "black", color: "white" }}>
+              {isEnumerated && <a style={{ textAlign: "left" }}>{key}</a>}
+            </div>
+
+            <Block
+              draggable
+              onDragStart={() => (draggedItem.current = key)}
+              onDragEnter={() => (draggedOverItem.current = key)}
+              OnDragEnd={sortElements}
+              onDragOver={(e) => e.preventDefault()}
+              line={key}
+              key={key}
+              id={block.id}
+              handleAdd={handleAdd}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
